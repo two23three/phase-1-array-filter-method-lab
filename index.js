9dbe5774ca2d31b2d9eff7b2342113191061c65e
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-03-26 15:32:43 +0300

    drivers

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+   const lowerCaseName= name.toLowerCase()
+  return drivers.filter((name)=> name.toLowerCase()===lowerCaseName)
+
+}
+function fuzzyMatch(drivers,letters){
+    return drivers.filter(driver=>driver.startsWith(letters))
+
+
+}
+function matchName(drivers,name){
+    return drivers.filter((drivers) => drivers.name === name)
+
+}
